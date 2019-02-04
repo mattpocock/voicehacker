bcd00fe1d2fe5acd2f0ad2867f9888c77b75ceae
matt <matt.pocock@thevirtualforge.com>
2019-02-04 18:17:30 +0000

    Added eslint disabler

--- backend/scripts/apolloServer/index.js
+++ backend/scripts/apolloServer/index.js
@@ -109,5 +109,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`); // eslint-disable-line
 });
