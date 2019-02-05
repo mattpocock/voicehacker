becd6442fd5f7db0e86bd0536e77b8d715b8b793
matt <matt.pocock@thevirtualforge.com>
2019-02-05 08:54:46 +0000

    Added some simple sound work to demo the accents

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,55 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import styled from 'styled-components/native';
+import { Button, Text } from 'react-native';
 
-export default () => (
+export default ({
+  data: {
+    allFile: { edges },
+  },
+}: Props) => (
   <Wrapper>
     <Title>Hello</Title>
+    {edges.map(({ node: { src, word } }) => (
+      <>
+        <Text>{word}</Text>
+        <audio src={src} controls />
+      </>
+    ))}
   </Wrapper>
 );
+
+interface Props {
+  data: {
+    allFile: {
+      edges: [
+        {
+          node: {
+            id: string;
+            word: string;
+            src: string;
+          };
+        }
+      ];
+    };
+  };
+}
+
+export const SOUND_QUERY = graphql`
+  {
+    allFile(
+      filter: { sourceInstanceName: { eq: "audio" }, name: { eq: "thank" } }
+    ) {
+      edges {
+        node {
+          id
+          word: name
+          src: publicURL
+        }
+      }
+    }
+  }
+`;
 
 const Title = styled.Text`
   font-size: 2rem;
